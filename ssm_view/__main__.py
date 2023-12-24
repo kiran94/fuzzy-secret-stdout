563b0cd72cd1cc912a8bcbe66b2457a4da4175b0
kiran94 <kiran_patel94@hotmail.com>
2023-12-24 09:21:06 +0000

    refactor(core): move ssm logic into integrations

--- ssm_view/__main__.py
+++ ssm_view/__main__.py
@@ -3,7 +3,13 @@ import boto3
 import logging
 
 from pyfzf.pyfzf import FzfPrompt
-from ssm_view.ssm import fetch_all_ssm_keys
+
+from ssm_view.integrations.aws_ssm import AWSParameterStore
+from ssm_view.models import SecretStoreItem
+
+from rich.live import Live
+from rich.spinner import Spinner
+from rich.text import Text
 
 
 LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', logging.WARN)
@@ -12,17 +18,21 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    ssm = boto3.client('ssm')
     search = FzfPrompt()
 
-    result = fetch_all_ssm_keys(ssm)
-    ssm_keys: list[str] = [x['Name'] for x in result]
+    # TODO: Move to Factory
+    aws_ssm = AWSParameterStore(boto3.client('ssm'))
+
+    with Live(Spinner('dots', text=Text('Loading')), transient=True):
+        result: list[SecretStoreItem] = aws_ssm.fetch_all()
+
+    ssm_keys: list[str] = [x.key for x in result]
 
     selected: list[str] = search.prompt(ssm_keys)
-    result = ssm.get_parameters(Names=selected, WithDecryption=True)
+    result: list[SecretStoreItem] = aws_ssm.fetch_secrets(selected)
 
-    for current_result in result['Parameters']:
-        print(current_result['Value'])
+    for current_result in result:
+        print(current_result.value)
 
 if __name__ == '__main__': # pragma: nocover
     main()
