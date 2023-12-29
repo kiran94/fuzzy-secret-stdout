cfc82c9754f9b609d152a5364959e09127b621e6
Kiran Patel <7103956+kiran94@users.noreply.github.com>
2023-12-29 14:17:32 +0000

    feat(integrations): support aws secret manager (#8)
    * feat(factory): add aws secret manager
    
    * refactor(integrations): abstract pagination logic
    
    * style(integrations): remove space
    
    * feat(integrations): add secret manager support
    
    * docs(readme): add integrations section
    
    * test(integrations): verify secret manager fetch secrets
    
    * chore(makefile): add additional runs for each integration

--- fuzzy_secret_stdout/integrations/aws_secret_manager.py
+++ fuzzy_secret_stdout/integrations/aws_secret_manager.py
@@ -6,7 +6,7 @@ from fuzzy_secret_stdout.integrations import SecretIntegration
 
 logger = logging.getLogger(__name__)
 
-class AWSParameterStore(SecretIntegration):
+class AWSSecretManager(SecretIntegration):
 
     def __init__(self, boto_client) -> None:
         self._boto_client = boto_client
@@ -14,11 +14,11 @@ class AWSParameterStore(SecretIntegration):
     def fetch_all(self, max_batch_results: Optional[int] = 3) -> list[SecretStoreItem]:
 
         def inner(boto_client, **kwargs):
-            return boto_client.describe_parameters(**kwargs)
+            return boto_client.list_secrets(**kwargs)
 
-        return self._paginate_results(inner, max_batch_results, 'Parameters', 'Name', 'ssm')
+        return self._paginate_results(inner, max_batch_results, 'SecretList', 'Name', 'secretmanager')
 
     def fetch_secrets(self, item_names: list[str]) -> list[SecretStoreItem]:
-        result = self._boto_client.get_parameters(Names=item_names, WithDecryption=True)
-        result = [SecretStoreItem(x['Name'], x['Value']) for x in result['Parameters']]
+        result = self._boto_client.batch_get_secret_value(SecretIdList=item_names)
+        result = [SecretStoreItem(x['Name'], x['SecretString']) for x in result['SecretValues']]
         return result
--- fuzzy_secret_stdout/integrations/factory.py
+++ fuzzy_secret_stdout/integrations/factory.py
@@ -3,10 +3,12 @@ from enum import Enum
 import boto3
 
 from fuzzy_secret_stdout.integrations.aws_ssm import AWSParameterStore
+from fuzzy_secret_stdout.integrations.aws_secret_manager import AWSSecretManager
 from fuzzy_secret_stdout.integrations import SecretIntegration
 
 class Integration(str, Enum):
     AWS_SSM = "AWS_SSM"
+    AWS_SECRET_MAN = "AWS_SECRET_MAN"
 
     @staticmethod
     def list_options() -> list[str]:
@@ -16,5 +18,7 @@ class Integration(str, Enum):
 def create_integration(integration: Integration) -> SecretIntegration:
     if integration == Integration.AWS_SSM:
         return AWSParameterStore(boto3.client('ssm'))
+    elif integration == Integration.AWS_SECRET_MAN:
+        return AWSSecretManager(boto3.client('secretsmanager'))
     else:
         raise NotImplementedError(f'integration {integration} not implemented')
