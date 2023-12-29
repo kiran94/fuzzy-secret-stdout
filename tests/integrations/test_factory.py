ac5e0134943e3cffaaf8f40e794a6ad08bc67d0d
kiran94 <kiran_patel94@hotmail.com>
2023-12-29 13:57:32 +0000

    feat(factory): add aws secret manager

--- tests/integrations/test_factory.py
+++ tests/integrations/test_factory.py
@@ -4,6 +4,7 @@ import pytest
 
 from fuzzy_secret_stdout.integrations.factory import create_integration, Integration
 from fuzzy_secret_stdout.integrations.aws_ssm import AWSParameterStore
+from fuzzy_secret_stdout.integrations.aws_secret_manager import AWSSecretManager
 
 @patch('fuzzy_secret_stdout.integrations.factory.boto3')
 def test_create_integration_ssm(mock_boto: Mock):
@@ -13,10 +14,17 @@ def test_create_integration_ssm(mock_boto: Mock):
     assert result._boto_client == mock_boto.client.return_value
     assert mock_boto.client.call_args_list == [call('ssm')]
 
+@patch('fuzzy_secret_stdout.integrations.factory.boto3')
+def test_create_integration_secretmanager(mock_boto: Mock):
+    result = create_integration(Integration.AWS_SECRET_MAN)
+
+    assert isinstance(result, AWSSecretManager)
+    assert result._boto_client == mock_boto.client.return_value
+    assert mock_boto.client.call_args_list == [call('secretsmanager')]
 
 def test_create_integration_unimplemented():
     with pytest.raises(NotImplementedError, match='integration DUMMY not implemented'):
         create_integration("DUMMY")
 
 def test_integration_list_options():
-    assert Integration.list_options() == ['AWS_SSM']
+    assert Integration.list_options() == ['AWS_SSM', 'AWS_SECRET_MAN']
