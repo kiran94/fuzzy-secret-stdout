44a9f24333522fe5d745df115c7762e45ab2c4d2
kiran94 <kiran_patel94@hotmail.com>
2024-01-20 09:07:02 +0000

    test(integrations): updated for new default max_batch_results value

--- tests/integrations/test_aws_ssm.py
+++ tests/integrations/test_aws_ssm.py
@@ -18,7 +18,7 @@ def test_fetch_all_no_parameters(describe_parameters_return: dict):
     result = integration.fetch_all()
 
     assert result == []
-    assert mock_ssm.describe_parameters.call_args_list == [call(MaxResults=3)]
+    assert mock_ssm.describe_parameters.call_args_list == [call(MaxResults=50)]
 
 
 @pytest.mark.parametrize("max_results", [
@@ -49,7 +49,7 @@ def test_fetch_all_ssm_keys_no_pagination():
     integration = AWSParameterStore(mock_ssm)
     result = integration.fetch_all()
 
-    assert mock_ssm.describe_parameters.call_args_list == [call(MaxResults=3)]
+    assert mock_ssm.describe_parameters.call_args_list == [call(MaxResults=50)]
     assert result == [
         SecretStoreItem(key='param1'),
         SecretStoreItem(key='param2'),
@@ -88,7 +88,7 @@ def test_fetch_all_ssm_keys_pagination():
         SecretStoreItem(key='param4')
     ]
 
-    assert mock_ssm.describe_parameters.call_args_list == [call(MaxResults=3), call(NextToken='token1', MaxResults=3), call(NextToken='token2', MaxResults=3)]
+    assert mock_ssm.describe_parameters.call_args_list == [call(MaxResults=50), call(NextToken='token1', MaxResults=50), call(NextToken='token2', MaxResults=50)]
 
 
 def test_fetch_secrets():
